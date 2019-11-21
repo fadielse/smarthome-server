1b25a5bd2c4f4f09a71ebf5688142f4b142df902
Fadilah Hasan <fadilah.hasan@metraplasa.co.id>
2019-11-21 13:21:42 +0700

    fix undeclared variable

--- smarthome-voice.py
+++ smarthome-voice.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from json import dumps
 import json
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 api = Api(app)
